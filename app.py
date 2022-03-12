defb07c5611ed2c0c5f4aa930d1e4acecc04e855
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-03-12 15:50:54 -0500

    Completed climate app code

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ Station = Base.classes.station
 # Flask Setup
 app = Flask(__name__)
 
+session = Session(engine)
 
 
 # Flask Routes
@@ -35,8 +36,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/[start date: YYYY-MM-DD]<br/>"
-        f"/api/v1.0/[start date: YYYY-MM-DD]/[end date: YYYY-MM-DD]<br/>" 
+        f"/api/v1.0/temp/<start><br/>"
+        f"/api/v1.0/temp/<start>/<end><br/>" 
     )
 
 
@@ -45,28 +46,38 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
+  
 
     """Return a list of all precipitation data"""
-    # Query all passengers
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016, 8, 23").all()
+    # Query all precipitation
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-23").order_by(Measurement.date.desc()).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_precipitation = list(np.ravel(results))
+#     all_precipitation = list(np.ravel(results))
 
-    return jsonify(all_precipitation)
+#     return jsonify(all_precipitation)
+
+# Create a dictionary from the row data and append to a list of all_passengers
+    all_precip = []
+    for date, prcp in results:
+        precip_dict = {}
+        precip_dict["date"] = date
+        precip_dict["prcp"] = prcp
+        all_precip.append(precip_dict)
+
+    return jsonify(all_precip)
+
 
 # Stations path
 
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
 
     """Return a list of all Stations"""
-    # Query all passengers
+    # Query all stations
     results = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
 
     session.close()
@@ -76,29 +87,46 @@ def stations():
 
     return jsonify(all_stations)
 
+# Tobs path
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+    """Return a list of all Dates and Temps from station USC00519281"""
+    # Query all Temps
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2016-08-23").filter(Measurement.station == "USC00519281").order_by(Measurement.date.desc()).all()
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    session.close()
 
-#     session.close()
+    # Convert list of tuples into normal list
+    all_tobs = list(np.ravel(results))
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
+    return jsonify(all_tobs)
 
-#     return jsonify(all_passengers)
+# Temp Start Date & Time Start Date/End Date Path
+# With assistance from TA, we combined the two API's with a if statement
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def start_date_temps(start=None,end=None):
+    if not end:
+
+         """Fetch the start date whose start matches
+           the path variable supplied by the user"""
+        # Query all Temps
+         results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+    else:
+         results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    session.close()
+       
+    
+
+    data_temps = list(np.ravel(results))
+
+    return jsonify(data_temps)
+
 
 
 
